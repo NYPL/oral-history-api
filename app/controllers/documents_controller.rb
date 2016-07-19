e405d15d17b0585a2a08820cf033d5071a71d6a6
Brian Foo <brian@youaremyjoy.org>
2016-07-19 17:13:54 -0400

    add filtering by collection, add fuzzy search, add parent search via child

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,10 +6,24 @@ class DocumentsController < ApplicationController
   end
 
   def search
+    @results = nil
     client = Elasticsearch::Client.new host: elastic_connection_string
+    index = Index.new
 
-    @resp = client.search q: params[:q]
-    render :json => @resp
+    # Get query filters
+    query = params[:q]
+    filters = nil
+    filters = params[:filters].to_unsafe_h if params[:filters].present?
+    puts "Query string: #{query}"
+    puts "Filters #{filters.inspect}"
+
+    # Do query
+    queryBody = index.getSearchBody(query, filters)
+    # puts queryBody.inspect
+
+    @results = client.search index: index.getDefaultName, type: 'item', body: queryBody
+
+    render :json => @results
   end
 
   def show
--- app/models/index.rb
+++ app/models/index.rb
@@ -4,6 +4,69 @@ class Index
     ENV['INDEX_NAME'] || 'primary'
   end
 
+  def self.validFilters
+    ["collection_id"]
+  end
+
+  def getFilters(f)
+    filters = []
+    valid_filters = Index.validFilters
+    if f.present?
+      valid_filters.each do |valid_filter_key|
+        if f.key?(valid_filter_key) && !f[valid_filter_key].blank?
+          filter = { term: {} }
+          filter[:term][:"#{valid_filter_key}"] = f[valid_filter_key]
+          filters << filter
+        end
+      end
+    end
+    filters
+  end
+
+  def getQuery(q)
+    query = ""
+    if q.present?
+      # Add fuzzy search to query
+      ngrams = q.gsub(/\s+/m, ' ').strip.split(" ")
+      ngrams = ngrams.map { |n| "#{n}~" }
+      query = ngrams.join(" ")
+    end
+    query
+  end
+
+  def getSearchBody(q, filters=nil)
+    query = getQuery(q)
+    filters = getFilters(filters)
+
+    # build query body
+    body = {
+      query: {
+        bool: {
+          should: [
+            {
+              match: { title: query }
+            },{
+              match: { description: query }
+            },{
+              has_child: { type: "annotation", query: { match: { text: query } }, inner_hits: {} }
+            },{
+              has_child: { type: "line", query: { match: { best_text: query } }, inner_hits: {} }
+            },{
+              has_child: { type: "line", query: { match: { original_text: query } }, inner_hits: { name: "original_line" } }
+            }
+          ]
+        }
+      }
+    }
+
+    # Add filters
+    if filters.length > 0
+      body[:query][:bool][:filter] = filters
+    end
+
+    body
+  end
+
   def getDefaultName
     Index.defaultName
   end
